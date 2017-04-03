206f8f678683f243d2d421686acf012a95bed7a9
zwq00000 <zwq00000@gmail.com>
2017-04-03 17:53:40 +0800

    Rebuild to dotnetcore

---
+++ src/Xunit.StaFact/ThisAssembly.cs
@@ -0,0 +1,6 @@
+﻿internal static class ThisAssembly {
+    internal const string AssemblyName = "Xunit.StaFact";
+    internal const string Title = "Xunit.StaFact.dll";
+    internal const string Description = "Xunit.StaFact.dll";
+    internal const string DefaultAlias = "Xunit.StaFact.dll";
+}
